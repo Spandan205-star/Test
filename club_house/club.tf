c9bafde68f1e07243528d4d8a20bc995979d4652
Spandan <SpandanMondal@niteshkushwaha9595fbdgmail.onmicrosoft.com>
2025-07-15 11:46:14 +0530

    add_rg_stg

---
+++ club_house/club.tf
@@ -0,0 +1,26 @@
+terraform {
+#
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.35.0"
+    }
+  }
+  backend "azurerm" {
+    
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "2266ad84-1142-42c3-ba58-b1ebdb118aaf"
+}
+
+module "resource_group" {
+  source = "C:/DevOpsInsiders/Batch17/azure-devsecops-batch-17/CodeSamples/Terraform/Class4/resource_group"
+}
+
+# module "storage_account" {
+#   depends_on = [module.resource_group]
+#   source     = "C:/DevOpsInsiders/Batch17/azure-devsecops-batch-17/CodeSamples/Terraform/Class4/storage_account"
+# }
